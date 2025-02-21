ca2ad40197f7a825ca07b62ce36da5ad7616072d
amar2mail9 <amar2mail9@gmail.com>
2025-02-21 09:44:40 +0530

    Delete Folder Sqaure

---
+++ Paractice01.cpp
@@ -0,0 +1,29 @@
+/*
+Practice 01
+
+* * * *
+* * * *
+* * * *
+* * * *
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // How much Time Want Print
+    int num = 4;
+
+    for (int i = 1; i <= num; i++)
+    {
+        for (int j = 0; j <= num; j++)
+        {
+            cout << "*" << " ";
+        }
+
+        cout << endl;
+    }
+}
